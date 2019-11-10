b9d6cfd8386254052356e5404fe61277edcc74a1
BareBeaverBat <ssiliss72@gmail.com>
2019-11-10 13:47:00 -0500

    added listener code stubs for buttons

--- jsrc/MainMenu.java
+++ jsrc/MainMenu.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainMenu
 {
@@ -9,4 +11,34 @@ private JTextArea isTrained;
 private JButton transcribeButton;
 private JScrollPane transcriptionOutput;
 private JTextArea chosenImageFiles;
+
+public MainMenu( )
+{
+	trainNeuralNetworkButton.addActionListener(new ActionListener()
+	{
+		@Override
+		public void actionPerformed( final ActionEvent e )
+		{
+			//TODO use Jython interpreter to invoke python code for training the NN's
+		}
+	});
+
+	chooseSourceImageButton.addActionListener(new ActionListener()
+	{
+		@Override
+		public void actionPerformed( final ActionEvent e )
+		{
+			//TODO create a JFileChooser to select the image file
+			// also set the chosenImageFiles text area to that file name
+		}
+	});
+	transcribeButton.addActionListener(new ActionListener()
+	{
+		@Override
+		public void actionPerformed( final ActionEvent e )
+		{
+			//TODO use Jython Interpreter to invoke python code to use the current NN's on the loaded image
+		}
+	});
+}
 }
