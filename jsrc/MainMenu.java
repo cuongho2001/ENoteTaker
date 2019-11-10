1a45d0ba0f67105a60847d92820e9cc857b70b53
BareBeaverBat <ssiliss72@gmail.com>
2019-11-10 13:35:33 -0500

    added prototype of GUI main menu without any logic behind it

---
+++ jsrc/MainMenu.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class MainMenu
+{
+private JPanel rootPanel;
+private JButton chooseSourceImageButton;
+private JButton trainNeuralNetworkButton;
+private JTextArea isTrained;
+private JButton transcribeButton;
+private JScrollPane transcriptionOutput;
+private JTextArea chosenImageFiles;
+}
