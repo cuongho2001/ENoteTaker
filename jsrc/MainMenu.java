3c1aebab4184846b847cfb124b10993524a8fc3f
Fayazud <fayazud.din@temple.edu>
2019-11-19 18:21:14 -0500

    added logic and ui for image selection

--- jsrc/MainMenu.java
+++ jsrc/MainMenu.java
@@ -1,6 +1,13 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.filechooser.FileSystemView;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class MainMenu
 {
@@ -11,7 +18,8 @@ private JTextArea isTrained;
 private JButton transcribeButton;
 private JTextArea chosenImageFiles;
 private JScrollPane imagePreviewsScrollWrapper;
-private JTextPane textPane1;
+private JTextPane outPutOfImage;
+private JLabel imageLabel;
 
 public MainMenu( )
 {
@@ -21,6 +29,10 @@ public MainMenu( )
 		public void actionPerformed( final ActionEvent e )
 		{
 			//TODO use Jython interpreter to invoke python code for training the NN's
+			//TODO create a JFileChooser to select the image file
+			// also set the chosenImageFiles text area to that file name
+			//TODO it might also be relatively easy to have an image preview pane which could show any number of images inside itself
+
 		}
 	});
 
@@ -29,13 +41,27 @@ public MainMenu( )
 		@Override
 		public void actionPerformed( final ActionEvent e )
 		{
-			//TODO create a JFileChooser to select the image file
-			// also set the chosenImageFiles text area to that file name
-			//TODO it might also be relatively easy to have an image preview pane which could show any number of images inside itself
+			JFileChooser j = new JFileChooser(FileSystemView.getFileSystemView());
 
-			//TESTING A COMMIT TO NEWLY CREATED BRANCH
+			//shows open dialog box
+			int r = j.showOpenDialog(null);
 
+			if ( r == JFileChooser.APPROVE_OPTION )
+			{
+				File fi = new File(j.getSelectedFile()
+									.getAbsolutePath());
+				BufferedImage image = null;
 
+				try
+				{
+					image = ImageIO.read(fi);
+					ImageIcon icon = new ImageIcon(image.getScaledInstance(150, 300, image.SCALE_FAST));
+					imageLabel.setIcon(icon);
+				} catch ( IOException ex )
+				{
+					ex.printStackTrace();
+				}
+			}
 		}
 	});
 	transcribeButton.addActionListener(new ActionListener()
@@ -44,7 +70,50 @@ public MainMenu( )
 		public void actionPerformed( final ActionEvent e )
 		{
 			//TODO use Jython Interpreter to invoke python code to use the current NN's on the loaded image
+
+			JFileChooser j = new JFileChooser(FileSystemView.getFileSystemView());
+
+			//shows open dialog box
+			int r = j.showOpenDialog(null);
+
+			// If the user selects a file
+			if ( r == JFileChooser.APPROVE_OPTION )
+			{
+				// Set the label to the path of the selected directory
+				File fi = new File(j.getSelectedFile()
+									.getAbsolutePath());
+
+				try
+				{
+					// String
+					String s1 = "", sl = "";
+
+					// File reader
+					FileReader fr = new FileReader(fi);
+
+					// Buffered reader
+					BufferedReader br = new BufferedReader(fr);
+
+					sl = br.readLine();
+
+					while ( (s1 = br.readLine()) != null )
+					{
+						sl = sl + "\n" + s1;
+					}
+
+					// Set the text
+					outPutOfImage.setText(sl);
+				} catch ( Exception evt )
+				{
+					JOptionPane.showMessageDialog(rootPanel, evt.getMessage());
+				}
+			}
 		}
 	});
+}
+
+private void createUIComponents( )
+{
+	// TODO: place custom component creation code here
 }
 }
