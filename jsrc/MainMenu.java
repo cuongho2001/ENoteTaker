b16fcfd9e0b9a2bde805934393015585258fdf5a
BareBeaverBat <ssiliss72@gmail.com>
2019-11-20 08:59:13 -0500

    added logic for java program to pass input file names to 'eval.py' Python script
    -tweaked eval code for flags to avoid (possibly machine-dependent) fatal error
    -tweaked eval code for loading vocab to avoid (possibly machine-dependent) fatal error
    -added intellij configuration for project having a python interpreter (I think this only works if the Python support plugin is installed)

--- jsrc/MainMenu.java
+++ jsrc/MainMenu.java
@@ -1,7 +1,8 @@
 import jep.Interpreter;
 import jep.JepConfig;
 import jep.JepException;
-import jep.SharedInterpreter;
+import jep.SubInterpreter;
+import jep.python.PyCallable;
 import jep.python.PyObject;
 
 import javax.swing.*;
@@ -12,6 +13,7 @@ import java.util.List;
 
 public class MainMenu
 {
+private final String programName = "EnoteTaker";
 private JPanel rootPanel;
 private JButton chooseSourceImageButton;
 private JButton trainNeuralNetworkButton;
@@ -26,6 +28,9 @@ public MainMenu( )
 {
 	isTrained.setText("Neural Network is not yet trained");
 
+	JepConfig jConfig = new JepConfig().addIncludePaths("./CNNRNN")
+									   .addIncludePaths("./CNNTXT");
+
 
 	trainNeuralNetworkButton.addActionListener(new ActionListener()
 	{
@@ -35,28 +40,15 @@ public MainMenu( )
 			isTrained.setText("Neural Network is now training");
 			rootPanel.update(rootPanel.getGraphics());
 
-
-			//TODO use Jython interpreter to invoke python code for training the NN's
-			JepConfig jConfig = new JepConfig().addIncludePaths("./CNNRNN")
-											   .addIncludePaths("./CNNTXT");
-			try
+			try ( Interpreter interp = new SubInterpreter(jConfig) )
 			{
-				SharedInterpreter.setConfig(jConfig);
-			} catch ( JepException ex )
-			{
-				System.err.println("Failed to configure the Python interpreter");
-				ex.printStackTrace();
-			}
-			try ( Interpreter interp = new SharedInterpreter() )
-			{
-				interp.exec("import train");
-
 				PyObject pySysObj = interp.getValue("sys", PyObject.class);
 
 				List<String> pyArgv = new ArrayList<>();
-				pyArgv.add("EnoteTaker");
+				pyArgv.add(programName);
 				pySysObj.setAttr("argv", pyArgv);
 
+				interp.exec("import train");
 				interp.exec("train.main()");
 
 				isTrained.setText("Neural Network is trained");
@@ -85,6 +77,48 @@ public MainMenu( )
 		public void actionPerformed( final ActionEvent e )
 		{
 			//TODO use Jython Interpreter to invoke python code to use the current NN's on the loaded image
+
+			try ( Interpreter interp = new SubInterpreter(jConfig) )
+			{
+				PyObject pySysObj = interp.getValue("sys", PyObject.class);
+
+				List<String> pyArgv = new ArrayList<>();
+				pyArgv.add(programName);
+				pySysObj.setAttr("argv", pyArgv);
+
+				String pyPosDataFilePath = "./CNNRNN/data/rt-polaritydata/rt-polarity.pos";
+				String pyNegDataFilePath = "./CNNRNN/data/rt-polaritydata/rt-polarity.neg";
+
+				interp.exec("import data_helpers");
+				interp.exec("from data_helpers import set_tf_flag");
+				PyCallable pyDefineStrFlag = interp.getValue("set_tf_flag", PyCallable.class);
+				pyDefineStrFlag.call("positive_input_data_file", pyPosDataFilePath,
+					"path of file with positive input data");
+				pyDefineStrFlag.call("negative_input_data_file", pyNegDataFilePath,
+					"path of file with negative input data");
+
+				interp.exec("import eval");
+
+				interp.exec("eval");
+
+				/*
+				interp.exec("import nbformat\n" +
+							"from nbconvert.preprocessors import ExecutePreprocessor");
+
+				interp.exec("ep = ExecutePreprocessor(timeout=600, kernel_name='python3')");
+
+				final String pyCnnPredictNotebookPath = "./CNNTXT/CNN/Model Predict.ipynb";
+				interp.set("notebookPath", pyCnnPredictNotebookPath);
+				interp.exec("with open(notebookPath) as f:\n" +
+							"    nb = nbformat.read(f, as_version=4)");
+				interp.exec("ep.preprocess(nb, {'metadata': {'path': 'notebooks/'}})");
+				 */
+
+			} catch ( JepException ex )
+			{
+				System.err.println("Python interpreter failed while running the neural network on new input data:");
+				ex.printStackTrace();
+			}
 		}
 	});
 }
